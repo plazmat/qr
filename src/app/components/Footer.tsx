b60041b9985138443df5b3cae312c0bc8a07a248
Marcin Opinc <marcin.opinc@gmail.com>
2025-03-07 12:48:11 +0100

    dodano inf o danych

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,16 +2,21 @@ import Link from "next/link"
 
 export default function Footer() {
   return (
-    <footer className="fixed bottom-0 right-0 p-4 text-sm text-gray-600">
-      Created with{" "}
-      <Link
-        href="https://aitrain.pl"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="font-bold text-indigo-600 hover:text-indigo-800 transition-colors"
-      >
-        AITRAIN
-      </Link>
+    <footer className="fixed bottom-0 left-0 right-0 p-4 flex justify-between items-center text-sm text-gray-600 bg-white bg-opacity-90">
+      <div className="ml-4">
+        Wszystkie operacje wykonywane są wyłącznie w Twojej przeglądarce. Dane wprowadzone w formularzu nie są przesyłane ani przechowywane na żadnym serwerze.
+      </div>
+      <div>
+        Created with{" "}
+        <Link
+          href="https://aitrain.pl"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="font-bold text-indigo-600 hover:text-indigo-800 transition-colors"
+        >
+          AITRAIN
+        </Link>
+      </div>
     </footer>
   )
 }
